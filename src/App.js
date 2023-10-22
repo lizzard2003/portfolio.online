356536f5c785ab5138f9f82df335e6fb5d885fd9
Liz DeLaRosa <59484240+lizzard2003@users.noreply.github.com>
2023-10-21 23:17:19 -0500

    push to netlify for update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useRef, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
@@ -6,6 +7,27 @@ import Portfolio from "./components/Portfolio";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
 function App() {
+    useEffect(() => {
+        // Load Google Analytics script asynchronously when the component mounts
+        const script = document.createElement("script");
+        script.async = true;
+        script.src = "https://www.googletagmanager.com/gtag/js?id=G-YWCLD7R153";
+        document.head.appendChild(script);
+
+        script.onload = () => {
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                window.dataLayer.push(arguments);
+            }
+            gtag("js", new Date());
+            gtag("config", "G-YWCLD7R153");
+        };
+
+        // Clean up script tag on component unmount
+        return () => {
+            document.head.removeChild(script);
+        };
+    }, []);
   return (
       <div>
         <NavBar/>
